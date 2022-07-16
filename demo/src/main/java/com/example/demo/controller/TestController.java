5c8c4c8decec629d65a66a685f0a1ac5cd4655af
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-07-16 22:32:58 +0900

    첫번재 commit 2

---
+++ demo/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+// 프로토타입 개발중 간단한 데이터 확인을 위한 test컨트롤러
+
+@RestController
+@Tag(name = "bear controller", description = "bear controller desc")
+public class TestController {
+
+	@GetMapping("/print")
+	public String helloworld() {
+		return "첫번째 test!";
+
+	}
+
+}
