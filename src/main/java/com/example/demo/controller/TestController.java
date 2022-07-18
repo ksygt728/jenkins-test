c4d79d828c36900efb5f4ed3bac2a3ce9be66077
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-07-18 22:03:38 +0900

    add sysout

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,18 +1,20 @@
 package com.example.demo.controller;
 
+import java.lang.System.Logger;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 // 프로토타입 개발중 간단한 데이터 확인을 위한 test컨트롤러
-
 @RestController
 @Tag(name = "bear controller", description = "bear controller desc")
 public class TestController {
 
 	@GetMapping("/print")
 	public String helloworld() {
+		System.out.println("원격 자동 빌드 유발");
 		return "build test5";
 
 	}
