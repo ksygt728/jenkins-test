8cca2afecdac88ecc5db38abdc47f11e025866c0
seungyeon <57320841+ksygt728@users.noreply.github.com>
2022-07-18 22:14:01 +0900

    불필요한 import 제거

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import java.lang.System.Logger;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
